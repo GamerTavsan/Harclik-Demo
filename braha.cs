3a51b2d8966b5033239875c3af5e4d966bbdee21
Emre Kaya <gamertavsan2005@gmail.com>
2021-04-26 15:36:22 +0300

    harçlık

---
+++ braha.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+
+void Update()
+    {
+        while (buton==true)
+        {
+            birikim = gelir/oran;
+        }
+    }
+
+int para,yüzdelik,sonuçyazı;
